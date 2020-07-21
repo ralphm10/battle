c7268ce8b6d7395e7535141ad91020c05ed52f64
Paul Humphreys <paulhumphreys81@aol.co.uk>
2020-07-21 16:49:32 +0100

    redirect

--- app.rb
+++ app.rb
@@ -1,19 +1,28 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
-    'Testing infrastructure working!'
     erb(:index)
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb(:play)
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
   end
 
   get '/test' do
     'Testing infrastructure working!'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb(:play)
   end
 
   run! if app_file == $0
