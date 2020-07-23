10dec8f38bf1377f296afbaf76a66a07210e91a0
Ralph Mallett <rmallett12@gmail.com>
2020-07-23 16:12:20 +0100

    Remove sessions and store player as global variable

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-
+require './lib/player'
 
 class Battle < Sinatra::Base
 
@@ -10,8 +10,8 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player1 = Player.new(params[:player_1_name])
+    $player2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
@@ -20,15 +20,15 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player1.name
+    @player_2_name = $player2.name
     erb(:play)
 
   end
 
   get '/confirmation' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player1.name
+    @player_2_name = $player2.name
     erb(:confirmation)
   end
 
